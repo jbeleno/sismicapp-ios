08b7261e19e7d0b028f0de0379e4f4c2504c5052
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-05-18 13:34:57 -0300

    Initial Commit

---
+++ sismicappTests/sismicappTests.swift
@@ -0,0 +1,36 @@
+//
+//  sismicappTests.swift
+//  sismicappTests
+//
+//  Created by Juan Beleño Díaz on 18/05/16.
+//  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
+//
+
+import XCTest
+@testable import sismicapp
+
+class sismicappTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
