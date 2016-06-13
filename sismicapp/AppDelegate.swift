0f9d64a1fa6780b59f3fc9152df2217f02e0a6ba
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-06-13 19:28:07 -0300

    New set of images in the tabs, color fix

--- sismicapp/AppDelegate.swift
+++ sismicapp/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Changing the tabBar color when the items are selected
+        UITabBar.appearance().tintColor = UIColor.init(colorLiteralRed: 244.0/255.0, green: 67.0/255.0, blue: 54.0/255.0, alpha: 1.0);
+        
         return true
     }
 
