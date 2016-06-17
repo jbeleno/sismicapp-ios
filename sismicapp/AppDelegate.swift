470d00fcc4c31d9a35e530732bbed20d31e49af4
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-06-17 14:31:24 -0300

    Some advances in notifications in AppDelegate

--- sismicapp/AppDelegate.swift
+++ sismicapp/AppDelegate.swift
@@ -2,12 +2,17 @@
 //  AppDelegate.swift
 //  sismicapp
 //
+//  This file is handling with changing the default color in the TabBar,
+//  it's handling with client side of notifications
+//
 //  Created by Juan Beleño Díaz on 18/05/16.
 //  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
 //
 
 import UIKit
 import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,39 +20,89 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         // Changing the tabBar color when the items are selected
-        UITabBar.appearance().tintColor = UIColor.init(colorLiteralRed: 244.0/255.0, green: 67.0/255.0, blue: 54.0/255.0, alpha: 1.0);
+        UITabBar.appearance().tintColor = UIColor.init(colorLiteralRed: 244.0/255.0,
+                                                       green: 67.0/255.0,
+                                                       blue: 54.0/255.0,
+                                                       alpha: 1.0)
+        
+        // Register for remote notifications >= iOS 8.0
+        let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+        application.registerForRemoteNotifications()
         
         // Initialize Firebase
         FIRApp.configure()
         
+        // Add observer for InstanceID token refresh callback.
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(self.tokenRefreshNotification),
+                                                         name: kFIRInstanceIDTokenRefreshNotification,
+                                                         object: nil)
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // [START receive_message]
+    func application(application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [NSObject : AnyObject],
+                     fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        
+        // Print message ID.
+        print("Message ID: \(userInfo["gcm.message_id"]!)")
+        
+        // Print full message.
+        print("%@", userInfo)
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // [END receive_message]
+    
+    
+    
+    // [START refresh_token]
+    func tokenRefreshNotification(notification: NSNotification) {
+        let refreshedToken = FIRInstanceID.instanceID().token()!
+        print("InstanceID token: \(refreshedToken)")
+        
+        // Connect to FCM since connection may have failed when attempted before having a token.
+        connectToFcm()
     }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // [END refresh_token]
+    
+    
+    
+    // [START connect_to_fcm]
+    func connectToFcm() {
+        FIRMessaging.messaging().connectWithCompletion { (error) in
+            if (error != nil) {
+                print("Unable to connect with FCM. \(error)")
+            } else {
+                print("Connected to FCM.")
+            }
+        }
     }
-
+    // [END connect_to_fcm]
+    
+    
+    
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        connectToFcm()
     }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    
+    
+    // [START disconnect_from_fcm]
+    func applicationDidEnterBackground(application: UIApplication) {
+        FIRMessaging.messaging().disconnect()
+        print("Disconnected from FCM.")
     }
-
+    // [END disconnect_from_fcm]
 
 }
 
--- sismicapp/HomeController.swift
+++ sismicapp/HomeController.swift
@@ -2,12 +2,24 @@
 //  HomeController.swift
 //  sismicapp
 //
+//  This file handles with the register/update of the device data,
+//  sends a new session, receive and show in the screen a set of post
+//  (seism data, ads, etc)
+//
 //  Created by Juan Beleño Díaz on 10/06/16.
 //  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
 //
 
 import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 class HomeController: UIViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
 }
